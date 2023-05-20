1abff1c7fa8691038520d80e859b7040abb8d114
JOÃO VICTOR ALVES MESSIAS <3275.343@edu.fb.org.br>
2023-05-19 21:52:52 -0300

    Primeiro Exemplo de comandos SQL

---
+++ ms-sql/aula1.slq.sql
@@ -0,0 +1,11 @@
+use AdventureWorks2012
+
+SELECT * FROM sys.tables
+SELECT * FROM Person.Person;
+SELECT FirstName FROM Person.Person
+
+SELECT  FirstName AS PrimeiroNome,
+		LastName AS UltimoNome
+FROM Person.Person;
+
+
